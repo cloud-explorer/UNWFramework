b74bd74e53ca1d1b8c8eeba19abd41daa278272a
rrangan <rrangan@gmail.com>
2014-08-17 22:49:46 -0400

    Added Validation Messages

--- UNWomen.Prototype.Client.Entities/Movie.cs
+++ UNWomen.Prototype.Client.Entities/Movie.cs
@@ -18,11 +18,16 @@ namespace UNWomen.Prototype.Client.Entities
         {
             public MovieValidator()
             {
-                RuleFor(obj => obj.Title).NotEmpty();
+                RuleFor(obj => obj.Title).NotEmpty()
+                    .Must(m => m.Length <= 200)
+                    .WithMessage("Title must not be empty and must be less than 200 characters");
                 RuleFor(obj => obj.ReleaseYear).NotEmpty()
                     .GreaterThan(2000)
-                    .LessThanOrEqualTo(DateTime.Now.Year + 1);
-                RuleFor(obj => obj.ImdbId).NotEmpty();
+                    .LessThanOrEqualTo(DateTime.Now.Year + 1)
+                    .WithMessage("Release must not be empty and must be greater than 2000 less than next year");
+                RuleFor(obj => obj.ImdbId).NotEmpty()
+                    .Must(m => m.Length <= 4)
+                    .WithMessage("IMDB Id must not be empty and must be less that 4 characters");
             }
         }
     }
